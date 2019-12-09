a2cdae2829f57a77575182fbd127aac7f72c58e1
bhoomikakhandelwal <48714561+bhoomikakhandelwal@users.noreply.github.com>
2019-12-09 11:03:06 +0530

    Remove spacing from todo

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -12,21 +12,29 @@ export default class Todo extends React.Component {
     if (todo.done) {
       return (
         <div>
-        <li className="list-items" >
-          <div className="circle-point"></div>
-         <p className="list-name"><a href="" onClick={this.done.bind(this) }><del>{todo.text}</del>{" "}</a></p>
-        </li>
-        <hr className="hr-line" />
+          <li className="list-items">
+            <div className="circle-point" />
+            <p className="list-name">
+              <a href="" onClick={this.done.bind(this)}>
+                <del>{todo.text}</del>
+              </a>
+            </p>
+          </li>
+          <hr className="hr-line" />
         </div>
       );
     } else {
       return (
         <div>
-        <li className="list-items">
-         <div className="circle-point"></div>
-        <p className="list-name"><a href="" onClick={this.done.bind(this) }>{todo.text}{" "}</a></p>
-        </li>
-        <hr className="hr-line" />
+          <li className="list-items">
+            <div className="circle-point" />
+            <p className="list-name">
+              <a href="" onClick={this.done.bind(this)}>
+                {todo.text}
+              </a>
+            </p>
+          </li>
+          <hr className="hr-line" />
         </div>
       );
     }
